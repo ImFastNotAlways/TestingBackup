5f7adcb9e9f7862f50c75c68b330fbf9eeae6656
Richard <zverins.lv@gmail.com>
2023-12-18 02:16:30 +0200

    peivienoju vel komentarus noderigs.py

--- noderigs.py
+++ noderigs.py
@@ -1,3 +1,6 @@
 ok ir pats galvenais
 
 
+es pat nepamaniju ka ieliku sadu pauzi
+jaatceras ari tas, ka komentariem vajadzetu but precizakiem, savadak apjuku
+un man tagad radas zalas atstarpites bet nevaru lietot garumz'imes.
