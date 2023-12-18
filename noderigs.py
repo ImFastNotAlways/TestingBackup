3f43f00e449d84c03fcce52bbfdc9734bfdfa5ed
Richard <zverins.lv@gmail.com>
2023-12-18 02:28:53 +0200

    ye vel komentari noderigs.py

--- noderigs.py
+++ noderigs.py
@@ -1,5 +1,6 @@
 ok ir pats galvenais
 
+parasti japagaida 30sec pirms citi branch redz, ka atpalikusi
 velejos vel piebilst, ka nevaig aizmirst git pull
 es pat nepamaniju ka ieliku sadu pauzi
 jaatceras ari tas, ka komentariem vajadzetu but precizakiem, savadak apjuku
