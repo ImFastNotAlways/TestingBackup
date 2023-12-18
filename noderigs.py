6c0816b8c7da57e9b493018eec2f474afb0562d4
Richard <zverins.lv@gmail.com>
2023-12-18 02:23:09 +0200

    pievienoju velvienu komentaru noderigs.py

--- noderigs.py
+++ noderigs.py
@@ -1,6 +1,6 @@
 ok ir pats galvenais
 
-
+velejos vel piebilst, ka nevaig aizmirst git pull
 es pat nepamaniju ka ieliku sadu pauzi
 jaatceras ari tas, ka komentariem vajadzetu but precizakiem, savadak apjuku
 un man tagad radas zalas atstarpites bet nevaru lietot garumz'imes.
